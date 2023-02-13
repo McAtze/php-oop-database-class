46c03be3ba3f7c890d7971609d229cdad9e32951
McAtze <info@it-maku.com>
2023-02-13 19:30:37 +0100

    Updated

--- DatabaseClass.php
+++ DatabaseClass.php
@@ -1,76 +1,117 @@
 <?php
 class DatabaseClass {	
 
-    private $connection = null;
-    
+    private ?PDO $connection = null;
+
     /**
      * This function is called everytime this class is instantiated
+     * @param string $dbhost
+     * @param string $dbname
+     * @param string $username
+     * @param string $password
+     * @throws Exception
      */
-    public function __construct($dbhost = 'localhost', $dbname = 'dbName', $username = 'userName', $password = '') {
+    public function __construct(
+        string $dbhost = 'localhost',
+        string $dbname = 'dbName',
+        string $username = 'userName',
+        string $password = '') {
         try {
-            $this->connection = new PDO("mysql:host={$dbhost};dbname={$dbname};", $username, $password);
+            $this->connection = new PDO("mysql:host=$dbhost;dbname=$dbname;", $username, $password);
             $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-        } catch(Exception $e) {
+        }
+        catch(Exception $e) {
             throw new Exception($e->getMessage());   
         }			
     }
-		
-    /** 
+
+    /**
      * Insert a row/s in a database table
+     * @param string $statement
+     * @param array $parameters
+     * @return bool|string
+     * @throws Exception
      */
-    public function insert($statement = '', $parameters = []) {
+    public function insert(string $statement = '', array $parameters = []): bool|string
+    {
         try {
             $this->executeStatement($statement, $parameters);
+
             return $this->connection->lastInsertId();
-        } catch(Exception $e) {
+        }
+        catch(Exception $e) {
             throw new Exception($e->getMessage());   
         }		
     }
 
     /**
      * Select a row/s in a database table
+     * @param string $statement
+     * @param array $parameters
+     * @return bool|array
+     * @throws Exception
      */
-    public function select( $statement = '', $parameters = []) {
+    public function select(string $statement = '', array $parameters = []): bool|array
+    {
         try {
             $stmt = $this->executeStatement($statement, $parameters);
+
             return $stmt->fetchAll();
-        } catch(Exception $e) {
+        }
+        catch(Exception $e) {
             throw new Exception($e->getMessage());   
         }		
     }
-		
+
     /**
      * Update a row/s in a database table
+     * @param string $statement
+     * @param array $parameters
+     * @throws Exception
      */
-    public function update($statement = '', $parameters = []) {
+    public function update(string $statement = '', array $parameters = []): void
+    {
         try {
             $this->executeStatement($statement, $parameters);
-        } catch(Exception $e) {
+        }
+        catch(Exception $e) {
             throw new Exception($e->getMessage());   
         }		
-    }		
-		
+    }
+
     /**
      * Remove a row/s in a database table
+     * @param string $statement
+     * @param array $parameters
+     * @throws Exception
      */
-    public function remove($statement = '', $parameters = []) {
+    public function remove(string $statement = '', array $parameters = []): void
+    {
         try {
             $this->executeStatement($statement, $parameters);
-        } catch(Exception $e) {
+        }
+        catch(Exception $e) {
             throw new Exception($e->getMessage());   
         }		
-    }		
-    
+    }
+
     /**
      * Execute statement
+     * @param string $statement
+     * @param array $parameters
+     * @return bool|PDOStatement
+     * @throws Exception
      */
-    private function executeStatement($statement = '', $parameters = []) {
+    private function executeStatement(string $statement = '', array $parameters = []): bool|PDOStatement
+    {
         try {
             $stmt = $this->connection->prepare($statement);
             $stmt->execute($parameters);
+
             return $stmt;
-        } catch(Exception $e) {
+        }
+        catch(Exception $e) {
             throw new Exception($e->getMessage());   
         }		
     }   
